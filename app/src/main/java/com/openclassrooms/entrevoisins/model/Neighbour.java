ee5c65fc17085f334ac74adb0a995e6ac61d4cd9
Clémençot Alexandre <73541197+Alexandre-C-89@users.noreply.github.com>
2023-02-17 15:18:55 +0100

    Ajout de l'attribut isFavorite.

--- app/src/main/java/com/openclassrooms/entrevoisins/model/Neighbour.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/model/Neighbour.java
@@ -35,13 +35,14 @@ public class Neighbour {
      * @param avatarUrl
      */
     public Neighbour(long id, String name, String avatarUrl, String address,
-                     String phoneNumber, String aboutMe) {
+                     String phoneNumber, String aboutMe, boolean isFavorite) {
         this.id = id;
         this.name = name;
         this.avatarUrl = avatarUrl;
         this.address = address;
         this.phoneNumber = phoneNumber;
         this.aboutMe = aboutMe;
+        this.isFavorite = false;
     }
 
     public long getId() {
