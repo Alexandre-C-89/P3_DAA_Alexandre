9faaa103ccf421c2a30e3f96e3fea7580077ee3e
Clémençot Alexandre <73541197+Alexandre-C-89@users.noreply.github.com>
2023-02-17 19:10:32 +0100

    Correction erreur nom fichier XML + création du fichier DetailNeighbourActivity

---
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/DetailNeighbourActivity.java
@@ -0,0 +1,8 @@
+package com.openclassrooms.entrevoisins.ui.neighbour_list;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class DetailNeighbourActivity extends AppCompatActivity {
+
+}
